3f10a95991e78b86be7c563a03bf2f998fca5c07
spadl <spadl@DESKTOP-3JCDJMG>
2026-09-23 21:54:01 +0200

    Dodaj pliki projektów.

---
+++ Gra w kości 2026 desktop/Kosc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Gra_w_kości_2026_desktop
+{
+    class Kosc
+    {
+        public static int liczbaInstancji = 0;
+
+        public string[] nazwyPlikow = { "kosc0.png", "kosc1.png", "kosc2.png",
+                                        "kosc3.png", "kosc4.png", "kosc5.png", "kosc6.png" };
+
+        public int liczbaOczek;
+        public int idPliku;
+        public bool dostepna;
+
+        static Random losowy = new Random();
+        public Kosc(int wartosc)
+        {
+            if (wartosc < 1 || wartosc > 6)
+                wartosc = 0;
+
+            liczbaOczek = wartosc;
+            idPliku = wartosc;
+            dostepna = true;
+            liczbaInstancji++;
+        }
+        public Kosc()
+        {
+            int wartosc = losowy.Next(1, 7);
+
+            liczbaOczek = wartosc;
+            idPliku = wartosc;
+            dostepna = true;
+            liczbaInstancji++;
+        }
+        public void Rzut()
+        {
+            if (dostepna)
+            {
+                int wartosc = losowy.Next(1, 7);
+                liczbaOczek = wartosc;
+                idPliku = wartosc;
+            }
+        }
+        public void Zablokuj()
+        {
+            dostepna = false;
+        }
+    }
+}
---
+++ Gra w kości 2026 desktop/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Gra_w_kości_2026_desktop
+{
+    public partial class MainWindow : Window
+    {
+        Kosc[] kosci = new Kosc[5];
+        Image[] obrazy;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            obrazy = new Image[] { obraz0, obraz1, obraz2, obraz3, obraz4 };
+            for (int i = 0; i < 5; i++)
+            {
+                kosci[i] = new Kosc(0);
+                PokazObraz(i);
+            }
+        }
+        private void Rzut_Click(object sender, RoutedEventArgs e)
+        {
+            int suma = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                kosci[i].Rzut();
+                PokazObraz(i);
+                suma = suma + kosci[i].liczbaOczek;
+            }
+
+            wynik.Text = suma.ToString();
+        }
+        private void Kosc_Click(object sender, MouseButtonEventArgs e)
+        {
+            Image obraz = (Image)sender;
+            int i = int.Parse(obraz.Tag.ToString());
+
+            if (kosci[i].dostepna)
+            {
+                kosci[i].Zablokuj();
+                obraz.Opacity = 0.5;
+            }
+            else
+            {
+                kosci[i].dostepna = true;
+                obraz.Opacity = 1;
+            }
+        }
+        private void PokazObraz(int i)
+        {
+            string plik = kosci[i].nazwyPlikow[kosci[i].idPliku];
+            obrazy[i].Source = new BitmapImage(new Uri("Images/" + plik, UriKind.Relative));
+        }
+    }
+}
